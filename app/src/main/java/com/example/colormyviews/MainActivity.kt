335bb82448541d5d50c160b586b23e973fd27a85
admin <123456>
2021-06-02 16:18:03 +0700

    apply view bindings

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -3,23 +3,15 @@ package com.example.colormyviews
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.colormyviews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private var boxOneText: TextView? = null
-    private var boxTwoText: TextView? = null
-    private var boxThreeText: TextView? = null
-    private var boxFourText: TextView? = null
-    private var boxFiveText: TextView? = null
-    private var redButton: Button? = null
-    private var greenButton: Button? = null
-    private var yellowButton: Button? = null
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setListeners()
     }
 
@@ -35,34 +27,26 @@ class MainActivity : AppCompatActivity() {
             R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
 
             // Boxes using custom colors for background
-            R.id.red_button -> boxThreeText?.setBackgroundResource(R.color.my_red)
-            R.id.yellow_button -> boxFourText?.setBackgroundResource(R.color.my_yellow)
-            R.id.green_button -> boxFiveText?.setBackgroundResource(R.color.my_green)
+            R.id.red_button -> binding.boxThreeText.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> binding.boxFourText.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> binding.boxFiveText.setBackgroundResource(R.color.my_green)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
 
     private fun setListeners() {
-
-        boxOneText = findViewById<TextView>(R.id.box_one_text)
-        boxTwoText = findViewById<TextView>(R.id.box_two_text)
-        boxThreeText = findViewById<TextView>(R.id.box_three_text)
-        boxFourText = findViewById<TextView>(R.id.box_four_text)
-        boxFiveText = findViewById<TextView>(R.id.box_five_text)
-        redButton = findViewById<Button>(R.id.red_button)
-        greenButton = findViewById<Button>(R.id.green_button)
-        yellowButton = findViewById<Button>(R.id.yellow_button)
-
         val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
-        val clickableViews: List<View?> =
-            listOf(
-                boxOneText, boxTwoText, boxThreeText,
-                boxFourText, boxFiveText, rootConstraintLayout,
-                redButton, greenButton, yellowButton
-            )
-        for (item in clickableViews) {
-            item?.setOnClickListener { makeColored(it) }
+        binding.apply {
+            val clickableViews: List<View?> =
+                listOf(
+                    boxOneText, boxTwoText, boxThreeText,
+                    boxFourText, boxFiveText, rootConstraintLayout,
+                    redButton, greenButton, yellowButton
+                )
+            for (item in clickableViews) {
+                item?.setOnClickListener { makeColored(it) }
+            }
         }
     }
 }

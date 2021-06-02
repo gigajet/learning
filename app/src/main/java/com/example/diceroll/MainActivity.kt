ff6048da30e6843cd10822113bedab1138ca1533
admin <123456>
2021-06-02 09:32:07 +0700

    to match kotlin convention

--- app/src/main/java/com/example/diceroll/MainActivity.kt
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -10,23 +10,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button)
-        roll_dice(findViewById(R.id.imageView))
-        roll_dice(findViewById(R.id.imageView2))
+        rollDice(findViewById(R.id.imageView))
+        rollDice(findViewById(R.id.imageView2))
         rollButton.setOnClickListener {
-            roll_dice(findViewById(R.id.imageView))
-            roll_dice(findViewById(R.id.imageView2))
+            rollDice(findViewById(R.id.imageView))
+            rollDice(findViewById(R.id.imageView2))
         }
     }
 
-    fun roll_dice(diceImageView: ImageView) {
-        val roll_result=Dice(6).roll()
+    fun rollDice(diceImageView: ImageView) {
+        val roll_result = Dice(6).roll()
         val drawableResult = when (roll_result) {
-            2->R.drawable.dice_2
-            1->R.drawable.dice_1
-            3->R.drawable.dice_3
-            4->R.drawable.dice_4
-            5->R.drawable.dice_5
-            else->R.drawable.dice_6
+            2 -> R.drawable.dice_2
+            1 -> R.drawable.dice_1
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
         diceImageView.setImageResource(drawableResult)
         diceImageView.contentDescription = roll_result.toString()
@@ -34,7 +34,5 @@ class MainActivity : AppCompatActivity() {
 }
 
 class Dice(val sides: Int) {
-    fun roll(): Int {
-        return (1..sides).random()
-    }
+    fun roll(): Int = (1..sides).random()
 }

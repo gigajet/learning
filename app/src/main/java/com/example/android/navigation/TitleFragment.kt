abc3b1f3e3ad141c5da0e3017cdc8e973ea4c30a
admin <123456>
2021-06-03 12:52:21 +0700

    feat: button clicks bug: back button navigation not handled properly

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -20,7 +21,10 @@ class TitleFragment : Fragment() {
             container,
             false
         )
-
+        binding.playButton.setOnClickListener { view: View ->
+            view.findNavController()
+                .navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())
+        }
         return binding.root
     }
 }
